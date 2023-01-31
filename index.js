79c9483ed97c0360830c0c67466ff1023cf3256a
Safaribust <122138882+Safaribust@users.noreply.github.com>
2023-02-01 01:45:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
 const axios = require('axios');
-const { createPool } = require('mysql');
+const mysql = require('mysql');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
@@ -15,67 +15,64 @@ const app = express();
 const server = http.createServer(app);
 
 const io = socketIo(server, {
-  cors: {
-    origin: ['http://localhost:3000', 'https://crash.safaribust.co.ke', '*'],
-  },
+cors: {
+origin: ['http://localhost:3000', 'https://crash.safaribust.co.ke', '*'],
+},
 });
 
-let pool;
-const connectPool = () => {
-  if (!pool) {
-   pool = createPool({
-  host: "173.214.168.54",
-  user: "bustadmin_dbadm",
-  password: ";,bp~AcEX,*a",
-  database: "bustadmin_paydb",
-  connectionLimit: 10
+let connection;
+const connectDb = () => {
+if (!connection) {
+connection = mysql.createConnection({
+host: "173.214.168.54",
+user: "bustadmin_dbadm",
+password: ";,bp~AcEX,*a",
+database: "bustadmin_paydb"
 });
-  }
-  return pool;
+}
+return connection;
 };
 
-const pooler = connectPool();
-
 io.on('connection', (socket) => {
-  console.log('Client connected: ', socket.id);
-  setInterval(() => getApiAndEmit(socket), 5000);
-  socket.on('disconnect', (reason) => {
-    console.log('Client disconnected:', reason);
-  });
+console.log('Client connected: ', socket.id);
+setInterval(() => getApiAndEmit(socket), 5000);
+socket.on('disconnect', (reason) => {
+console.log('Client disconnected:', reason);
+});
 });
 
 const getApiAndEmit = async (socket) => {
-  try {
-    pooler.getConnection(async (err, connection) => {
-      if (err) throw err;
-      console.log('Client disconnected waiting for mysql');
-      connection.query('SELECT * FROM transaction WHERE processed = 0', async (err, result) => {
-        if (err) throw err;
-          console.log('Client disconnected waiting for mysql==========',result);
-        Object.keys(result).forEach(async (key) => {
-          const row = result[key];
-          const transaction = await Transaction.findOne({ trans_id: row.trans_id });
-            console.log(transaction);
-          if (transaction) {
-            console.log('No new transactions');
-            const response = { deposited: false };
-            io.sockets.emit('FromAPI2', response);
-            connection.release();
-            return;
-          } else {
-            connection.query(`UPDATE transaction SET processed = 1 WHERE trans_id = "${row.trans_id}"`, (err, result) => {
-              if (err) throw err;
-              connection.release();
-            });
-            const trans = new Transaction({
-              type: 'Deposit',
-              trans_id: row.trans_id,
-              bill_ref_number: row.bill_ref_number,
-              trans_time: row.trans_time,
-              amount: row.trans_amount,
-              phone: row.bill_ref_number,
-              floatBalance: row.org_balance,
-            });
+try {
+connectDb().connect((err) => {
+if (err) throw err;
+console.log('Client disconnected waiting for mysql');
+connection.query('SELECT * FROM transaction WHERE processed = 0', async (err, result) => {
+if (err) throw err;
+console.log('Client disconnected waiting for mysql==========',result);
+Object.keys(result).forEach(async (key) => {
+const row = result[key];
+const transaction = await Transaction.findOne({ trans_id: row.trans_id });
+console.log(transaction);
+if (transaction) {
+console.log('No new transactions');
+const response = { deposited: false };
+io.sockets.emit('FromAPI2', response);
+connection.end();
+return;
+} else {
+connection.query(UPDATE transaction SET processed = 1 WHERE trans_id = "${row.trans_id}", (err, result) => {
+if (err) throw err;
+connection.end();
+});
+const trans = new Transaction({
+type: 'Deposit',
+trans_id: row.trans_id,
+bill_ref_number: row.bill_ref_number,
+trans_time: row.trans_time,
+amount: row.trans_amount,
+phone: row.bill_ref_number,
+floatBalance: row.org_balance,
+});
                await trans.save().then(async(item)=>{
                   try{
           
