487343009685fe95f82ec29fd597123e16cfb3c6
Safaribust <122138882+Safaribust@users.noreply.github.com>
2023-01-31 17:16:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -37,75 +37,69 @@ io.on("connection", (socket) => {
     console.log("client disconnected",reason);
   });
 });
-
-const getApiAndEmit = (socket) => {
-  try{
-      var con = mysql.createConnection({ 
+ var con = mysql.createConnection({ 
          host: "173.214.168.54",
          user: "bustadmin_dbadm",
          password: ";,bp~AcEX,*a",
          database:"bustadmin_paydb"
        }); 
-         con.connect((err)=> {
-           if (err) throw err;
-               con.query(`SELECT * FROM transaction WHERE processed=0 `, (err, result) =>
-               {
-               if (err) throw err;                       
-            Object.keys(result).forEach(async (key)=> { 
-               var row = result[key];
-               const transaction= await Transaction.findOne({trans_id:row.trans_id})
-               console.log(transaction);
-               if(!transaction){
-                 con.query(`UPDATE transaction SET processed = 1 WHERE trans_id = "${row.trans_id}"`,function(err,result){
-                   if(err) throw err;
-                   con.end();
-                
-                  })
-                  const trans= new Transaction({
-                    type:"Deposit",
-                        trans_id:row.trans_id,
-                        bill_ref_number:row.bill_ref_number,
-                        trans_time:row.trans_time,
-                        amount:row.trans_amount,
-                        phone: row.bill_ref_number,
-                        floatBalance:row.org_balance
-                  })
+const getApiAndEmit = (socket) => {
+           try{
+                  con.connect(function(err) {
+                    if (err) throw err;
+                        con.query(`SELECT * FROM transaction WHERE processed=0 `, function (err, result) {
+                        if (err) throw err;                     
+                        Object.keys(result).forEach(async function(key) {
+                        var row = result[key];
+                        const transaction= await Transaction.findOne({trans_id:row.trans_id})
+                        if(transaction){
+                          console.log("no new transactions");
+                          const response = {deposited: false};                            
+                          io.sockets.emit("FromAPI2", response);
+                          con.end();
+                          return
+                        }
+                        con.query(`UPDATE transaction SET processed = 1 WHERE trans_id = "${row.trans_id}"`,function(err,result){
+                              if(err) throw err;
+                               con.end();
+                            })
+                          const trans= new Transaction({
+                                    type:"Deposit",
+                                    trans_id:row.trans_id,
+                                    bill_ref_number:row.bill_ref_number,
+                                    trans_time:row.trans_time,
+                                    amount:row.trans_amount,
+                                    phone: row.bill_ref_number,
+                                    floatBalance:row.org_balance
+                              })
 
-             await trans.save().then(async(item)=>{
-                try{
-        
-                const account = await Account.findOne({ phone:row.bill_ref_number});
-                account.balance=parseFloat(+account?.balance) + parseFloat(+row.trans_amount)
-                await account.save()
-                console.log(account);
-                const response = {
-                  deposited: true,
-                  trans_id:row.trans_id
-                };
-                io.sockets.emit("FromAPI2", response);
-                }catch(err){
-                  console.log(err)
-                }
-              })
-            }else{
-              const response = {deposited: false};                          
-              io.sockets.emit("FromAPI2", response);
-              return con.end(()=>console.log("connection closed"))
-               
-               }
-             
-  
-          
-         
+                         await trans.save().then(async(item)=>{
+                            try{
+                    
+                            const account = await Account.findOne({ phone:row.bill_ref_number});
+                            account.balance=parseFloat(+account?.balance) + parseFloat(+row.trans_amount)
+                            await account.save()
+                            const response = {
+                                      deposited: true,
+                                      trans_id:row.trans_id
+                                    };
+                            io.sockets.emit("FromAPI2", response);
+                            }catch(err){
+                              console.log(err)
+                            }
+                         })
+                  
+                     });
+                })
+                
             });
-       })
-       
-   });
-    
- }catch(err){
- console.log(err)
-}
+   
+          }catch(err){
+          console.log(err)
+      }
 };
+
+
  
 const MONGO_URI =  "mongodb+srv://Safaribust:safaribust@cluster0.yuiecha.mongodb.net/?retryWrites=true&w=majority";    
 const PORT = process.env.PORT || 8050;
